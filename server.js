8ba1feb4dd2dfedc9e5f5fe8c83ca43579b05111
firdavsyangiev@gmail.com <firdavsyangiev@gmail.com>
2026-04-22 23:58:54 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ console.log("Web serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
 
 // 1: Kirish code
 app.use(express.static("public"));
@@ -15,17 +16,13 @@ app.set("views", "views");
 app.set("view engine", "ejs"); //BSSR
 
 // 4: Rooting code
-// app.get("/hello", (req, res) => {
-//   res.send(`<h1>Hello World</h1>`);
-// });
-
-// app.get("/gift", (req, res) => {
-//   res.send(`<h1>Siz sovg'alar bo'limidasiz</h1>`);
-// });
-
 app.post("/create-item", (req, res) => {
-  console.log(req.body);
-  res.json({ test: "success" });
+  // console.log(req.body);
+  // res.json({ test: "success" });
+});
+
+app.get("/author", (req, res) => {
+  res.render("author");
 });
 
 app.get("/", function (req, res) {
