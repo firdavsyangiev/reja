2aa33b9298cc4f9c0112d959f5021fe3703f7c1d
firdavsyangiev@gmail.com <firdavsyangiev@gmail.com>
2026-04-28 11:51:45 +0900

    feat: MITASK task-B

--- train.js
+++ train.js
@@ -1,3 +1,18 @@
+//    >>>>>>>> TASK <<<<<<<
+function countDigits(num) {
+  let count = 0;
+
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] >= "0" && num[i] <= "9") {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+console.log(countDigits("ad2a54y79wet0sfgb9"));
+
 // >>>>>>>> Async function <<<<<<<<
 
 // console.log("Jack Ma maslahatlari:");
@@ -66,40 +81,40 @@
 
 // >>>>>>>> Callback Function <<<<<<<<
 
-console.log("Jack Ma maslahatlari:");
-const list = [
-  "Yaxshi talaba bo'ling", // 0-20
-  "To'g'ri boshliq tanlang va ko'proq xato qiling", // 20-30
-  "Ustingizda ishlashni boshlang", // 30-40
-  "Siz kuchli bo'lgan narsalarni qiling", // 40-50
-  "Yoshlarga investitsiya qiling", // 50-60
-  "Endi dam oling! Foydasi yo'q endi)", // 60
-];
+// console.log("Jack Ma maslahatlari:");
+// const list = [
+//   "Yaxshi talaba bo'ling", // 0-20
+//   "To'g'ri boshliq tanlang va ko'proq xato qiling", // 20-30
+//   "Ustingizda ishlashni boshlang", // 30-40
+//   "Siz kuchli bo'lgan narsalarni qiling", // 40-50
+//   "Yoshlarga investitsiya qiling", // 50-60
+//   "Endi dam oling! Foydasi yo'q endi)", // 60
+// ];
 
-function maslahatBering(a, callback) {
-  if (typeof a !== "number") callback("Insert a number", null);
-  else if (a <= 20) callback(null, list[0]);
-  else if (a > 20 && a <= 30) callback(null, list[1]);
-  else if (a > 30 && a <= 40) callback(null, list[2]);
-  else if (a > 40 && a <= 50) callback(null, list[3]);
-  else if (a > 50 && a <= 60) callback(null, list[4]);
-  else {
-    setTimeout(function () {
-      callback(null, list[5]);
-    }, 5000);
-  }
-}
+// function maslahatBering(a, callback) {
+//   if (typeof a !== "number") callback("Insert a number", null);
+//   else if (a <= 20) callback(null, list[0]);
+//   else if (a > 20 && a <= 30) callback(null, list[1]);
+//   else if (a > 30 && a <= 40) callback(null, list[2]);
+//   else if (a > 40 && a <= 50) callback(null, list[3]);
+//   else if (a > 50 && a <= 60) callback(null, list[4]);
+//   else {
+//     setTimeout(function () {
+//       callback(null, list[5]);
+//     }, 5000);
+//   }
+// }
 
-console.log("Passed here 0");
-maslahatBering(65, (err, data) => {
-  if (err) console.log("ERROR:", err);
-  else {
-    console.log("Javob:", data);
-  }
-});
-console.log("Passed here 1");
+// console.log("Passed here 0");
+// maslahatBering(65, (err, data) => {
+//   if (err) console.log("ERROR:", err);
+//   else {
+//     console.log("Javob:", data);
+//   }
+// });
+// console.log("Passed here 1");
 
-//    >>>>>>>> A-TASK <<<<<<<
+//    >>>>>>>> TASK-A <<<<<<<
 // function countLetter(letter, word) {
 //   let count = 0;
 
