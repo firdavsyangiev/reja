bfb8d98109efee67fa9b01ebfb6e8c34f234562c
firdavsyangiev@gmail.com <firdavsyangiev@gmail.com>
2026-04-28 11:13:03 +0900

    feat: CRUD operations

--- app.js
+++ app.js
@@ -29,7 +29,16 @@ app.set("view engine", "ejs"); //BSSR
 
 // 4: Rooting code
 app.post("/create-item", (req, res) => {
-  //   // console.log(req.body);
+  console.log("user entered /create-item");
+  const new_reja = req.body.reja;
+  db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end("Something went wrong");
+    } else {
+      res.end("Succesfully added");
+    }
+  });
 });
 
 app.get("/author", (req, res) => {
@@ -37,7 +46,17 @@ app.get("/author", (req, res) => {
 });
 
 app.get("/", function (req, res) {
-  res.render("reja");
+  console.log("user entered /");
+  db.collection("plans")
+    .find()
+    .toArray((err, data) => {
+      if (err) {
+        console.log(err);
+        res.end("Something went wrong");
+      } else {
+        res.render("reja", { items: data });
+      }
+    });
 });
 
 module.exports = app;
